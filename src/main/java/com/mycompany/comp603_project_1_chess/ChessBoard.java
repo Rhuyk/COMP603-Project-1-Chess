19f486ce5b7f2f30a7af8dbf1a99f02b36ed06c8
rh200 <rh200@192.168.1.69>
2023-04-20 01:33:55 +1200

    Some foundations added

--- src/main/java/com/mycompany/comp603_project_1_chess/ChessBoard.java
+++ src/main/java/com/mycompany/comp603_project_1_chess/ChessBoard.java
@@ -9,5 +9,8 @@ package com.mycompany.comp603_project_1_chess;
  * @author rh200
  */
 public class ChessBoard {
-    //All pieces location, to be updated.
+    private enum Column {a,b,c,d,e,f,g,h};
+    private int[][] locations = new int[8][8]; // int[column][row]
+    
+    
 }
