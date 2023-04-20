863adab78b16ac9d3d0ce066562aeec92b8e332d
PJ <goinmc@gmail.com>
2023-04-20 21:28:55 +1200

    ChessBoard Start up pieces created.
    ChessGame main method created.
    
    startBoard() method fully finished.

--- src/main/java/Chess_Project_1/ChessBoard.java
+++ src/main/java/Chess_Project_1/ChessBoard.java
@@ -9,8 +9,8 @@ package Chess_Project_1;
  * @author rh200
  */
 public class ChessBoard {
-    //private enum Column {a,b,c,d,e,f,g,h};
-    private Piece[][] board; // int[column][row]
+
+    private Piece[][] board; // int[row][column]
     
     public ChessBoard()
     {
@@ -19,27 +19,61 @@ public class ChessBoard {
     
     public void startBoard()
     {
-        board[0][0] = new Rook(ChessPieceColour.WHITE,0,0);
-        board[0][1] = new Knight(ChessPieceColour.WHITE, 0, 1);
+        board[7][4] = new King(ChessPieceColour.WHITE,7,4);
+        board[0][4] = new King(ChessPieceColour.BLACK,0,4);
+        
+        board[7][3] = new Queen(ChessPieceColour.WHITE,7 ,3);
+        board[0][3] = new Queen(ChessPieceColour.BLACK,0 ,3);
+        
+        board[7][2] = new Bishop(ChessPieceColour.WHITE,7,2);
+        board[7][5] = new Bishop(ChessPieceColour.WHITE,7,5);
+        board[0][5] = new Bishop(ChessPieceColour.BLACK,0,5);
+        board[0][2] = new Bishop(ChessPieceColour.BLACK,0,2);
+        
+        board[7][0] = new Rook(ChessPieceColour.WHITE,7,0);
+        board[7][7] = new Rook(ChessPieceColour.WHITE,7,7);
+        board[0][7] = new Rook(ChessPieceColour.BLACK, 0, 7);
+        board[0][0] = new Rook(ChessPieceColour.BLACK, 0, 0);
+        
+        board[7][1] = new Knight(ChessPieceColour.WHITE,7,1);
+        board[7][6] = new Knight(ChessPieceColour.WHITE,7,6);
+        board[0][1] = new Knight(ChessPieceColour.WHITE,0,1);
+        board[0][6] = new Knight(ChessPieceColour.WHITE,0,6);
+        
+        for(int i = 0; i < 8; i++)
+        {
+            board[1][i] = new Pawn(ChessPieceColour.WHITE,1,i);
+            board[6][i] = new Pawn(ChessPieceColour.WHITE,6,i);
+        }
     }
     
     public void printBoard()
     {
+        int chessRows = 8;
+        
+        System.out.println("Chess Board: \n");
+        System.out.println("     a   b   c   d   e   f   g   h\n");
+        
         for(Piece[] row: board)
         {
+            System.out.println("   +---+---+---+---+---+---+---+---+");
+            System.out.print(chessRows + "  | ");
             for(Piece piece: row)
             {
+                
                 if(piece == null)
                 {
-                    System.out.println("- ");
+                    System.out.print("- | ");
                 }
                 else
                 {
-                    System.out.println(piece.getSymbol() +" ");
+                    System.out.print(piece.getSymbol() +" | ");
                 }
             }
-            System.out.println(8 - row[0].getRow());
+            System.out.println(" "+chessRows);
+            chessRows--;
         }
-        System.out.println("a b c d e f g h");
+        
+        System.out.println("\n     a   b   c   d   e   f   g   h");
     }
 }
--- src/main/java/Chess_Project_1/ChessPieces.java
+++ src/main/java/Chess_Project_1/ChessPieces.java
@@ -10,26 +10,26 @@ package Chess_Project_1;
  */
 public class ChessPieces {
     
-    public Piece king;
-    public Piece queen;
-    public Piece bishop1;
-    public Piece bishop2;
-    public Piece knight1;
-    public Piece knight2;
-    public Piece rook1;
-    public Piece rook2;
-    public Piece pawn1;
-    public Piece pawn2;
-    public Piece pawn3;
-    public Piece pawn4;
-    public Piece pawn5;
-    public Piece pawn6;
-    public Piece pawn7;
-    public Piece pawn8;
-    
-    public ChessPieces()
-    {
-        
-    }
+//    public Piece king;
+//    public Piece queen;
+//    public Piece bishop1;
+//    public Piece bishop2;
+//    public Piece knight1;
+//    public Piece knight2;
+//    public Piece rook1;
+//    public Piece rook2;
+//    public Piece pawn1;
+//    public Piece pawn2;
+//    public Piece pawn3;
+//    public Piece pawn4;
+//    public Piece pawn5;
+//    public Piece pawn6;
+//    public Piece pawn7;
+//    public Piece pawn8;
+//    
+//    public ChessPieces()
+//    {
+//        
+//    }
 
 }
