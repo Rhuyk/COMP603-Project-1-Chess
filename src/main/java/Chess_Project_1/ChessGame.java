97f3a9bde5e306fb24778617884eaf414af3f19b
PJ <goinmc@gmail.com>
2023-04-24 01:55:15 +1200

    Tested out getAvaliablemoves method in main method.

--- src/main/java/Chess_Project_1/ChessGame.java
+++ src/main/java/Chess_Project_1/ChessGame.java
@@ -15,19 +15,75 @@ public class ChessGame {
     {
         PiecesOnBoard board = new PiecesOnBoard();
         
-        board.movePiece(1, 1, 1, 3);
-        board.movePiece(0, 1, 0, 2);
-        board.movePiece(0, 2, 0, 3);
-        board.movePiece(0, 0, 0, 1);
-        board.movePiece(1, 0, 2, 2);
-        board.movePiece(2, 0, 0, 2);
         
+        // Test out avaliable moves for pawn. 
+        boolean[][] moves = board.getPiece(3, 1).getAvailableMoves();
+        System.out.println("Available moves for white Pawn at (3, 1):");
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 8; j++) 
+            {
+                if (moves[i][j]) 
+                {
+                    System.out.println("(" + i + ", " + j + ")");
+                }
+            }  
+        }
         
+        //Test out moving pawns
+          board.movePiece(1, 1, 1, 3);
+//        board.movePiece(0, 1, 0, 2);
+//        board.movePiece(0, 2, 0, 3);
+//        board.movePiece(0, 0, 0, 1);
+//        board.movePiece(1, 0, 2, 2);
+//        board.movePiece(2, 0, 0, 2);
+          board.movePiece(0, 1, 0, 3);
+        
+        // Test out creating players
         Player player1 = new Player(ChessPieceColour.WHITE,"PJ");
         Player player2 = new Player(ChessPieceColour.BLACK,"Jamar");
         
+        // Test out board flip for both players
         PrintBoard.printBoard(board,player1);
-        PrintBoard.printBoard(board,player2);
+//        PrintBoard.printBoard(board,player2);
+
+        moves = board.getPiece(4, 6).getAvailableMoves();
+        System.out.println("Available moves for black Pawn at (4, 6):");
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 8; j++) 
+            {
+                if (moves[i][j]) {
+                    System.out.println("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        
+        moves = board.getPiece(0, 0).getAvailableMoves();
+        System.out.println("Available moves for white Rook at (0, 0):");
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 8; j++) 
+            {
+                if (moves[i][j]) {
+                    System.out.println("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        
+        moves = board.getPiece(2, 0).getAvailableMoves();
+        System.out.println("Available moves for white Bishop at (2, 0):");
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 8; j++) 
+            {
+                if (moves[i][j]) {
+                    System.out.println("(" + i + ", " + j + ")");
+                }
+            }
+        }
+        
+        
         
     }
 }
