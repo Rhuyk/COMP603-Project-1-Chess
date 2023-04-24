d799344459bb289af631ac5c6322cc4e0b81369a
PJ <goinmc@gmail.com>
2023-04-25 00:04:19 +1200

    ChessBoardFileIO methods has been split up into smaller methods.

--- src/main/java/Chess_Project_1/ChessBoardFileIO.java
+++ src/main/java/Chess_Project_1/ChessBoardFileIO.java
@@ -21,64 +21,41 @@ public class ChessBoardFileIO {
     
     public static void saveGameForUser(String username, PiecesOnBoard board) throws FileNotFoundException, IOException 
     {
-    String filename = "chessData.txt";
-    boolean overwrite = false;
-    String line;
-    StringBuilder fileDataBuilder = new StringBuilder();
+        String filename = "chessData.txt";
+        boolean overwrite = false;
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) 
-    {
-        boolean userFound = false;
-
-        while ((line = reader.readLine()) != null) 
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) 
         {
-            if (username.equals(line)) 
-            {
-                System.out.println("Player Name already exists in the file.");
-                System.out.print("Do you wish to overwrite (Y/N): ");
-                Scanner scanner = new Scanner(System.in);
-                String choice = scanner.nextLine();
+            boolean userFound = checkIfUserExists(username, reader);
 
-                if (choice.equalsIgnoreCase("Y")) 
-                {
-                    overwrite = true;
-                } 
-                else 
+            if (userFound) 
+            {
+                overwrite = getOverwriteChoice();
+                if (!overwrite) 
                 {
                     System.out.println("Game not saved.");
                     return;
                 }
             }
-        }
         } 
         catch (IOException e) 
         {
-            System.out.println("Error: Game file could not be loaded!");
+            System.out.println("Chess game can not be loaded!");
+            return;
         }
 
         if(overwrite) 
         {
-            String fileData = fileDataBuilder.toString();
-            String[] lines = fileData.split("\n");
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) 
-            {
-                for (String fileLine : lines) 
-                {
-                    if (!fileLine.startsWith(username)) 
-                    {
-                        writer.write(fileLine + "\n");
-                    }
-                }
-            } 
-            catch (IOException e) 
-            {
-                System.out.println("Error deleting user data from file: " + filename);
-                return;
-            }
+            deleteUserDataFromFile(username, filename);
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) 
+        saveUserDataToFile(username, board);
+    }
+    
+    public static void saveUserDataToFile(String username, PiecesOnBoard board)
+    {
+        String filename = "chessdata.txt";
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) 
         {
             writer.write(username + "\n");
 
@@ -100,7 +77,7 @@ public class ChessBoardFileIO {
         } 
         catch (IOException e) 
         {
-            System.out.println("Error saving game: " + filename);
+            System.out.println("Chess game can not be saved!" + filename);
         }
     }
 
@@ -111,45 +88,31 @@ public class ChessBoardFileIO {
         PiecesOnBoard board = new PiecesOnBoard();
         
         try {
-            BufferedReader reader;
-            reader = new BufferedReader(new FileReader(filename));
-            String line;
-            StringBuilder gameDataBuilder = new StringBuilder();
-
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
             boolean userFound = false;
+            String gameData = "";
+            String line;
+
             while ((line = reader.readLine()) != null) 
             {
                 if(username.equals(line))
                 {
-                        userFound = true;
+                    userFound = true;
                 }
 
                 if (line.equals("###")) 
                 {
-                    board.clear();
-                    userFound = true;
-                    String gameData = gameDataBuilder.toString();
-                    String[] lines = gameData.split("\n");
-                    
-                for (String gameLine : lines) 
-                {
-                    String[] parts = gameLine.split(" ");
-                    if (parts.length >= 3) 
-                    { 
-                        String symbol = parts[0];
-                        int row = Integer.parseInt(parts[1]);
-                        int col = Integer.parseInt(parts[2]);
-
-                        Piece piece = createPiece(symbol, col, row);
-                        board.setPiece(col, row, piece);
+                    if (userFound) 
+                    {
+                        System.out.println("Game file has been loaded!");
+                        return board;
                     }
-                }
-                    gameDataBuilder = new StringBuilder();
+                    gameData = "";
                     userFound = false;
                 } 
                 else 
                 {
-                    gameDataBuilder.append(line).append("\n");
+                    gameData += line + "\n";
                 }
             }
 
@@ -157,40 +120,56 @@ public class ChessBoardFileIO {
         } 
         catch (IOException e) 
         {
-            System.out.println("Error: Game file could not be loaded!");
+            System.out.println("Chess game can not be loaded!");
         }
         
         return board;
     }
 
-    private static Piece createPiece(String symbol,int column,int row) {
-        switch (symbol) {
-            case "wP":
-                return new Pawn(ChessPieceColour.WHITE,column,row);
-            case "wR":
-                return new Rook(ChessPieceColour.WHITE,column,row);
-            case "wN":
-                return new Knight(ChessPieceColour.WHITE,column,row);
-            case "wB":
-                return new Bishop(ChessPieceColour.WHITE,column,row);
-            case "wQ":
-                return new Queen(ChessPieceColour.WHITE,column,row);
-            case "wK":
-                return new King(ChessPieceColour.WHITE,column,row);
-            case "bP":
-                return new Pawn(ChessPieceColour.BLACK,column,row);
-            case "bR":
-                return new Rook(ChessPieceColour.BLACK,column,row);
-            case "bN":
-                return new Knight(ChessPieceColour.BLACK,column,row);
-            case "bB":
-                return new Bishop(ChessPieceColour.BLACK,column,row);
-            case "bQ":
-                return new Queen(ChessPieceColour.BLACK,column,row);
-            case "bK":
-                return new King(ChessPieceColour.BLACK,column,row);
-            default:
-                return null;
+    private static boolean checkIfUserExists(String username, BufferedReader reader) throws IOException 
+    {
+        String line;
+
+        while ((line = reader.readLine()) != null) 
+        {
+            if (username.equals(line)) 
+            {
+                System.out.println("Player Name already exists in the file.");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean getOverwriteChoice() 
+    {
+        System.out.print("Do you wish to overwrite (Y/N): ");
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        return choice.equalsIgnoreCase("Y");
+    }
+
+    private static void deleteUserDataFromFile(String username, String filename) throws IOException 
+    {
+        StringBuilder fileDataBuilder = new StringBuilder();
+        String fileData = fileDataBuilder.toString();
+        String[] lines = fileData.split("\n");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) 
+        {
+            for (String fileLine : lines) 
+            {
+                if (!fileLine.startsWith(username)) 
+                {
+                    writer.write(fileLine + "\n");
+                }
+            }
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Chess game can not be deleted: !" + filename);
         }
     }
 }
